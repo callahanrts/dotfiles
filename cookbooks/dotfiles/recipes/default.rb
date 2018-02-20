0174c7504a0c1f7fc0ac75ec22edd2a8c3209694
Cody <callahanrts@gmail.com>
2018-02-19 19:11:54 -0800

    updates

--- cookbooks/dotfiles/recipes/default.rb
+++ cookbooks/dotfiles/recipes/default.rb
@@ -4,3 +4,16 @@
 #
 # Copyright (c) 2018 The Authors, All Rights Reserved.
 
+
+[
+  :exa,
+  :rbenv,
+  :the_silver_searcher,
+  :ripgrep
+].each do |package|
+  bash "Install #{package.to_s}" do
+    code "brew install #{package.to_s}"
+    not_if { !`brew ls --versions #{package.to_s}`.match(package.to_s).nil? }
+  end
+end
+
