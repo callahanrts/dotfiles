9d7b9daabd7639f9c405a5ead2dd569e315ff532
Cody <callahanrts@gmail.com>
2018-11-19 09:44:55 -0800

    remove unused

--- config/doom/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; ~/.config/doom/packages.el
-
-(package! fill-column-indicator)
-
-; (package! column-marker :recipe (:fetcher github :repo "emacsmirror/column-marker"))
