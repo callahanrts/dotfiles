48ea089a991b4b01605b5c563212dffdaa8c3d57
Cody <callahanrts@gmail.com>
2019-04-18 21:07:40 -0700

    Update background changer

--- dots/.zsh/new_bg.sh
+++ dots/.zsh/new_bg.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/.wallpapers
 pushd ~/.wallpapers
 rm ~/.wallpapers/*.png
-wget -O /home/cody/.wallpapers/background.png https://source.unsplash.com/2880x1800/
+wget -O /home/cody/.wallpapers/background.png https://source.unsplash.com/2880x1800/?nature
+mogrify -format png /home/cody/.wallpapers/background.png
 feh --bg-scale ~/.wallpapers/background.png
 popd
